aab649cad0cc9484140209a99e5d322462e1b69e
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2025-05-19 15:06:42 +0100

    Use source layout and switch to flit build backend.

---
+++ coverage-fixup.py
@@ -0,0 +1,14 @@
+# stdlib
+import re
+import sqlite3
+
+conn = sqlite3.connect(".coverage")
+c = conn.cursor()
+
+for (idx, filename) in c.execute("SELECT * FROM file").fetchall():
+	new_filename = re.sub(r"\.tox/.*/site-packages/", "src/", filename)
+	print(idx, filename, "->", new_filename)
+	c.execute("UPDATE file SET path=? WHERE id=?", (new_filename, idx))
+
+conn.commit()
+conn.close()
